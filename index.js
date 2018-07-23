249185ab4eabb6176f1c9d40760a49b1a4607699
sidfubo1 <sidfubo1@gmail.com>
2018-07-23 23:16:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Write your solution in this file!
+let driver = {
+  FirstName: 'Sid',
+  LastName: 'Shah',
+  Age: 25
+}
+
+function updateDriverWithKeyAndValue (driver, key, value) {
+  let driverCopy = {...driver};
+  driverCopy[key] = value;
+  return driverCopy;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
+  driver[key] = value;
+  return driver;
+}
+
+function deleteFromDriverByKey(driver, key) {
+  let driverCopy = {...driver};
+  delete driverCopy[key];
+  return driverCopy;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key) {
+  delete driver[key];
+  return driver;
+}
+
+
+
+
+
+
