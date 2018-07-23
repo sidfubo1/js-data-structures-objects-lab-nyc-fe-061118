2d208ed48035b6be26d7eac304be160a02ca0f5f
sidfubo1 <sidfubo1@gmail.com>
2018-07-23 23:16:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function destructivelyUpdateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function deleteFromDriverByKey(driver, key) {
-  delete driver[key];
-  return driver;
+  let driverCopy = {...driver};
+  delete driverCopy[key];
+  return driverCopy;
 }
